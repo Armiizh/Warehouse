20f65b79bdb977a4ef14d6cb6ab0245a06e23410
Artemii Zh <zhulkin2013@yandex.ru>
2024-09-10 20:39:22 +0400

    Finish

--- app/src/main/java/com/example/ip_test_task/data/dao/DaoItem.kt
+++ app/src/main/java/com/example/ip_test_task/data/dao/DaoItem.kt
@@ -1,8 +1,6 @@
 package com.example.ip_test_task.data.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.ip_test_task.data.model.Item
 import kotlinx.coroutines.flow.Flow
@@ -12,15 +10,9 @@ interface DaoItem {
 
     @Query("SELECT * FROM item")
     fun getAllItemsFlow(): Flow<List<Item>>
-    
+
     @Query("SELECT * FROM item")
     suspend fun getAllItems(): List<Item>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllItems(item: List<Item>)
-
-    @Insert
-    suspend fun insertItem(item: Item)
 
     @Query("UPDATE item SET amount = :newAmount WHERE id = :id")
     suspend fun updateById(id: Int, newAmount: Int)
